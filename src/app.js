70e1d3edd51636171b7ff2ce414b1b476e7b9a82
JJ Palacios <funihao@gmail.com>
2018-03-30 14:46:34 +0200

    First Challenge

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
 import './css/movies.styl'
 var html = require('./index.pug')
 
-console.log('Movi.es Layout')
+// var select = document.getElementById('.burger')
+
+var menu = $('#menu')
+var bar = $('.bar')
+
+menu.click(function () {
+  var burger = $( this ).find('.burger')
+  var user = $( this ).find('.user')
+
+  burger.toggleClass('open')
+  user.toggleClass('view')
+})
+
+bar.click(function () {
+
+  if ( $('.post').css('grid-column-start') === '1' ) {
+    $('.post').css('grid-column-start', '2')
+    bar.css('left', '100px')
+  }
+  else {
+    $('.post').css('grid-column-start', '1')
+    bar.css('left', '0px')
+
+  }
+})
